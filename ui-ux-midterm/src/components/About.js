6a3e85c63e1d1834736800f84da350a57225d576
AkshaPatel <akshpatel564@gmail.com>
2023-04-06 19:35:15 -0400

    added home page and about page content

--- ui-ux-midterm/src/components/About.js
+++ ui-ux-midterm/src/components/About.js
@@ -1,9 +1,84 @@
-
+import './css/About.css';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Ling from './images/JeanieLingMd.jpeg';
 
 export default function About() {
     return (
         <>
-            <h1> About </h1>
+        <div className='doctors'>
+            <h1> Meet Our Specialists </h1>
+                  
+            <Container sx={{ py: 8 }} maxWidth="md">
+                <Grid container spacing={4}>
+                    <Grid sx={{px:1.5}} xs={12} sm={6} md={4}>
+                        <Card
+                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            image="https://jerdeeye.com/wp-content/uploads/2018/05/EricJerde_headshot_web-resized.jpg"
+                            alt="Eye Doctor"
+                        />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Eric White OD
+                            </Typography>
+                            <Typography>
+                                University of Miami
+                            </Typography>
+                        </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid sx={{px:1.5}} xs={12} sm={6} md={4}>
+                        <Card
+                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            image="https://jerdeeye.com/wp-content/uploads/2018/05/Dr-Thill-Headshot-819x1020.jpg"
+                            alt="Eye Doctor"
+                        />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Ashley Jones OD
+                            </Typography>
+                            <Typography>
+                                John Hopkins University
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+
+                        </CardActions>
+                        </Card>
+                    </Grid>
+
+                    <Grid sx={{px:1.5}} xs={12} sm={6} md={4}>
+                        <Card
+                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            image={Ling}
+                            alt="Eye Doctor"
+                            />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                            Jeanie Ling OD
+                            </Typography>
+                            <Typography>
+                                St. Louis University
+                            </Typography>
+                        </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
         </>
     );
 }
--- ui-ux-midterm/src/components/Home.js
+++ ui-ux-midterm/src/components/Home.js
@@ -1,9 +1,34 @@
+import './css/Home.css';
 
 
 export default function Home() {
     return (
         <>
-            <h1> Home </h1>
+            <div className='slogan'>
+                <h1> Clear Vision Starts Here </h1>           
+                <strong> 
+                    Your vision care is our top priority! 
+                    Our doctors are board certified to treat cataracts, glaucoma,
+                    dye eyes, and perform LASIK.
+                </strong>
+            </div>
+            
+            <div className='contact'> 
+                <h1> Contact Us! </h1>
+                <div className='location'>
+                    <div className='info'>
+                        <h3> Address: </h3>
+                        <p> 
+                            8626 Queens Blvd, Elmhurst, NY 11373, United States
+                        </p>
+                    </div>
+                    <div className='info'>
+                        <h3> Phone: </h3>
+                        <p> (929)-488-7753 </p>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
